9a496ccdd26b460dad14c3f7e73d9454cd05145a
Trey50Daniel <tagg8945@gmail.com>
2016-08-30 16:55:42 -0500

    Added new Classes for game structure and player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player {
+
+    private int kills, deaths, ammo;
+    private float health, speed;
+    private int teamID, killStreak, modelID;
+    private PickUp[] pickUps;
+
+    public Player()
+    {
+        kills = 0;
+        deaths = 0;
+        ammo = 0;
+        killStreak = 0;
+        health = 100;
+        speed = 2.5f;
+        pickUps = new PickUp[99];
+    }
+}
