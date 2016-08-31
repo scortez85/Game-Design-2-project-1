228a4257c6cb5d55820c64312f6eb3cd1d13bf57
Trey50Daniel <tagg8945@gmail.com>
2016-08-31 13:14:46 -0500

    Added Scripts + Updated Scripts with vital data

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -1,8 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PickUp : MonoBehaviour {
+public class PickUp {
+    //private GameObject pickUp;
+    private string pickUpName;
 
+    public PickUp(string name)
+    {
+        pickUpName = name;
+    }
+    public void addPickUp(GameObject game)
+    {
+        Object.Instantiate(game);
+    }
+
+    public void spawnPickUp()
+    {
+
+    }
 	// Use this for initialization
 	void Start () {
 	
