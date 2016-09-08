e1a67864e167cb584431c686e4b80a643c3b5fda
Steven Cortez <steven_cortez@msn.com>
2016-09-07 21:44:11 -0500

    changes to camera and cinematic cam

---
+++ Assets/fpsCam.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fpsCam : MonoBehaviour {
+
+    public GameObject player;
+	void Start () {
+        
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        transform.position = player.transform.position;
+        transform.rotation = player.transform.rotation;
+    }
+}
