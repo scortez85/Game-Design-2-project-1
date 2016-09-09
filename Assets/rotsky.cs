e48938a6464e33eccdd0e408211f6bbd2d2b6100
Steven Cortez <steven_cortez@msn.com>
2016-09-09 01:13:11 -0500

    stuff

---
+++ Assets/rotsky.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotsky : MonoBehaviour {
+
+	//speed value for moving the skybox
+	public float speed;
+	//sky color change
+	public Renderer skyshade;
+	void Start () {
+	//this is for sky color
+		skyshade = GetComponent<Renderer> ();
+		//skyshade.material.shader
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//move that sky
+		transform.Rotate (0, speed * Time.deltaTime, 0);
+	
+	}
+}
